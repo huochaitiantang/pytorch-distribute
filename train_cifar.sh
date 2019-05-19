6f6c2435193fea1252a2db900380f27bc01d955c
liuliang <1432249204@qq.com>
2019-05-19 10:31:53 +0800

    tidy cifar

--- train_cifar.sh
+++ train_cifar.sh
@@ -2,7 +2,7 @@
 #/bin/bash
 
 # WHATEVER IN IFCONFIG (like eth0 or something
-export GLOO_SOCKET_IFNAME=enp0s31f6
+export GLOO_SOCKET_IFNAME=enp3s0
 
 world_size=1
 batch_size=192
@@ -10,12 +10,12 @@ dataset=cifar
 arch=alexnet
 rank=0
 
-python cifar_resnet.py \
+python train_cifar.py \
     --rank $rank \
     --world_size $world_size \
-    --ip 10.2.1.251 \
+    --ip 219.224.168.78 \
     --port 22000 \
     --batch_size $batch_size \
     --epochs 10 \
     --arch $arch \
-    --log exp/log_${dataset}_${arch}_world${world_size}_rank${rank}_batch${batch_size}.txt
+    --log log/log_${dataset}_${arch}_world${world_size}_rank${rank}_batch${batch_size}.txt
